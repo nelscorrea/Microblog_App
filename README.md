d36fcf88fcc6284085c04f77cef07b35430c5b76
saasbook <saasbook@saasbook.(none)>
2012-07-13 07:52:47 -0700

    Initial Microblog_app commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Ruby on Rails Tutorial - Microblog_app
+
+This is a Microblog app (sample_app) from the [*Ruby on Rails Tutorial*](http://railstutorial.org) by [Michael Hartl](http://michaelhartl.com).
+
+Developed for deployment on Heroku.
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MicroblogApp::Application.config.secret_token = '3e79deaefc06a13b522e9a1e2477908829f3dfab4006875e6345264faa9588dc9bfc08f9b1cd9a4ba3f769099f2c7eef93289d323a8ebccf8159493fe11e7dba'
